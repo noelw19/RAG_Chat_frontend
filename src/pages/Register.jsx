1e26e09861afb4906ac1bf5bb18827a397dcbfc0
noelw19 <noelw19@outlook.com>
2025-04-12 11:49:46 +1200

    add name to register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { API_ROUTES } from "../config/apiConfig";
 import axios from "axios";
 
 const Register = () => {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -15,7 +15,7 @@ const Register = () => {
       await axios.post(API_ROUTES.register, form);
       // window.location.href = "/login";
     } catch (error) {
-      alert("Error registering");
+      alert("Error registering: "+error);
     }
   };
 
@@ -23,8 +23,9 @@ const Register = () => {
     <div className="p-10 max-w-md mx-auto">
       <h2 className="text-2xl mb-4">Register</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
+        <input name="name" placeholder="Name" onChange={handleChange} className="p-2 border w-full text-white" />
         <input name="email" placeholder="Email" onChange={handleChange} className="p-2 border w-full text-white" />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} className="p-2 border w-full" />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} className="p-2 border w-full text-white" />
         <button className="p-2 bg-blue-600 text-white w-full">Register</button>
       </form>
     </div>
