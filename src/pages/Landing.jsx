51baad2ad25b76386749712551c12b9bdf47770a
noelw19 <noelw19@outlook.com>
2025-04-12 12:45:18 +1200

    working

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { getUser } from "@/utils/axios";
+import { getUser } from "../utils/axios";
 import { useEffect, useState } from "react";
 
 const Landing = () => {
--- uploader.sh
+++ uploader.sh
@@ -1,4 +1,4 @@
 npm run build 
 
 scp -r -i ~/.ssh/id_rsa ./dist root@170.64.152.229:~
-ssh -i ~/.ssh/id_rsa root@170.64.152.229 'bash -s < /deploy_rag_frontend.sh'
+ssh -i ~/.ssh/id_rsa root@170.64.152.229 'bash -s < ~/deploy_rag_frontend.sh'
